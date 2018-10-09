e6d4103e7b04d2c5022b858011218f01a8638a44
Administrator <Administrator@LAB01>
2018-10-09 01:49:32 -0700

    First commit

---
+++ Model/ToDoItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Model
+{
+    public class ToDoItem
+    {
+        public string Description { get; set; }
+        public int Index { get; set; }
+        public bool Completed { get; set; }
+        public string Category { get; set; }
+
+
+        public ToDoItem(string description)
+        {
+            Description = description;
+            Index = 0;
+        }
+
+        public static ToDoItem Load(string description, int index, bool completed)
+        {
+            ToDoItem newItem = new ToDoItem(description)
+            {
+                Index = index,
+                Completed = completed,
+                Category = "none"
+            };
+
+            return newItem;
+        }
+    }
+}
